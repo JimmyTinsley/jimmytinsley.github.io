2e17ae8b4d3568a3e3ea5452ec09e2434136bef1
JimmyTinsley <liujie333333@gmail.com>
2019-04-24 14:14:20 +0800

    update

--- src/proxy/gen-tld/gen.sh
+++
@@ -1,5 +0,0 @@
-curl -o ./.list.dat https://publicsuffix.org/list/effective_tld_names.dat
-
-node gen.js > ../src/tld-data.js
-
-echo "done"
--- src/proxy/release.sh
+++
@@ -1,3 +0,0 @@
-webpack \
-  --o ../../www/x.js \
-  --mode production
