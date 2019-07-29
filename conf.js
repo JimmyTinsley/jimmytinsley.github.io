c862a75fb0f937a6323e5437edaf4d1d3e8de412
JimmyTinsley <liujie333333@gmail.com>
2019-07-29 08:55:58 +0800

    Update front-end to newest version

--- conf.js
+++ conf.js
@@ -1,11 +1,13 @@
 jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
-  ver: '78',
+  // 每次修改配置，该值需要增加，否则不会生效。
+  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
+  ver: '110',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 20
+    ver: 62
   },
 
   // 节点配置
@@ -14,9 +16,8 @@ jsproxy_config({
       label: '演示服务-香港节点',
       lines: {
         // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
         'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 2,
       }
     },
     'demo-sg': {
@@ -25,16 +26,10 @@ jsproxy_config({
         'node-aliyun-sg.etherdream.com:8443': 1,
       },
     },
-    'AWS-SGP': {
-      label: '演示服务-新加坡节点',
-      lines: {
-       ['3.1.211.191.nip.io:8443']: 1,
-      },
-    },
     'mysite': {
       label: '当前站点',
       lines: {
-      	[location.host]: 1,
+        [location.host]: 1,
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -43,15 +38,14 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 6,
+        'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'a.007.workers.dev': 1,
-        'a.hehe.workers.dev': 1,
-        'a.lulu.workers.dev': 1,
-        'shrill-unit-8594.jsproxy.workers.dev': 1,
-        'proxy.jimmytinsley.workers.dev': 1,
+        'b.007.workers.dev': 1,
+        'b.hehe.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -59,8 +53,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  // node_default: 'mysite',
-  node_default: /jsproxy\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: 'mysite',
+  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -75,6 +69,12 @@ jsproxy_config({
 
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/',
+
+  // 首页路径
+  index_path: 'index_v3.html',
+
+  // 支持 CORS 的站点列表（实验中...）
+  direct_host_list: 'cors_v1.txt',
 
   /**
    * 自定义注入页面的 HTML
