28f42edf3c87d77d5457d92fa2099ae7f9e03a69
Jie Liu <liujie333333@gmail.com>
2019-07-29 09:29:15 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -65,10 +65,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  // assets_cdn: 'assets/',
+  assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
